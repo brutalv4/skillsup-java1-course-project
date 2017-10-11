5e2064fe9f0dc86811b68ddfeca0fba9cef7259a
Vladimir Bredikhin <brutal.v4@gmail.com>
2017-10-11 14:28:11 +0300

    fixed errors in test data statements

--- dao/src/main/resources/test-data.sql
+++ dao/src/main/resources/test-data.sql
@@ -1,8 +1,8 @@
 INSERT INTO PERSON
   (FIRST_NAME, LAST_NAME, BIRTH_DATE, NICKNAME)
-VAL);UES
+VALUES
   ('Vladimir', 'Bredihin', '1985-02-05', 'Nickname?');
 INSERT INTO PERSON
 (FIRST_NAME, LAST_NAME, BIRTH_DATE, NICKNAME)
 VALUES
-  ('Julia', 'Bredihina', '1986-06-11', 'Nickname?'
+  ('Julia', 'Bredihina', '1986-06-11', 'Nickname?');
